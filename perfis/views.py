318957204e24f8dbc5dd6af3d26e9bd3f9bf4cc8
Camila Carvalho <75643855+camilacarvalhon@users.noreply.github.com>
2022-07-06 22:54:06 -0300

    modificando o arquivo views.py para funcionar com a estrutura de uma API

--- perfis/views.py
+++ perfis/views.py
@@ -1,46 +1,77 @@
+
 from django.shortcuts import render, redirect
 from perfis.models import Perfil, Convite
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.decorators import permission_required
 
+from rest_framework import viewsets , response, status, exceptions
+from rest_framework.decorators import api_view, render_classes
+from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer
+from rest_framework.permissions import AllowAny
 
-@login_required
-def index(request):
+class PerfilViewSet(viewsets.ModelViewSet):
+    queryset = Perfil.objects.all()
+    serializer_class = None
 
-    print(request.user.username) #novo
-    print(request.user.email) #novo
-    print(request.user.has_perm('perfis.add_convite')) #novo
-    
-    
-    return render(request, 'index.html', { 'perfis' : Perfil.objects.all(),'perfil_logado' : get_perfil_logado(request)})
+    def get_serializer_class(self):
 
-@login_required
-def exibir(request, perfil_id):
-    
-     # precisa agora buscar do banco
-    perfil = Perfil.objects.get(id=perfil_id)
-    perfil_logado = get_perfil_logado(request)
-    ja_e_contato = perfil in perfil_logado.contatos.all()
-    tem_permissao= request.user.has_perm('perfis.add_convite')
+        if self.request.method == 'GET':
+            return None
 
-    return render(request, 'perfil.html', {'perfil' : perfil, 'perfil_logado' : get_perfil_logado(request), 'ja_e_contato' : ja_e_contato, 'tem_permissao':tem_permissao})
+        return super().get_serializer_class()
 
-@permission_required('perfis.add_convite', raise_exception=True)
-@login_required
-def convidar(request, perfil_id):
-    perfil_a_convidar = Perfil.objects.get(id=perfil_id)
-    perfil_logado = get_perfil_logado(request)
-    perfil_logado.convidar(perfil_a_convidar)
+    def get_permissions(self):
 
-    # realizando redirecionamento
-    return redirect('index')
+        if self.request.method == 'POST':
+            return (AllowAny(),) 
+        return super().get_permissions()
 
-@login_required
+@api_view(['GET'])   
+@render_classes((JSONRenderer, BrowsableAPIRenderer))
+def get_convites(request, *args, **kwargs):
+	perfil_logado = get_perfil_logado(request)
+	convites = Convite.objects.filter(convidado=perfil_logado)
+	serializer = None
+	return response.Response(serializer.data, status=status.HTTP_200_OK)
+
+
+
+@api_view(['POST'])
+@renderer_classes((JSONRenderer, BrowsableAPIRenderer))
+def convidar(request, *args, **kwargs):
+	try: 
+		perfil_a_convidar = Perfil.objects.get(id=kwargs['perfil_id'])
+	except:
+		raise exceptions.NotFound('Não foi encontrado um perfil com o id informado.')
+	perfil_logado = get_perfil_logado(request)
+	if perfil_a_convidar != perfil_logado:
+		perfil_logado.convidar(perfil_a_convidar)
+		return response.Response({
+			'messagem': f'Convite enviado com sucesso para {perfil_a_convidar.email}.'},
+			status=status.HTTP_201_CREATED)
+	raise exceptions.ParseError('Você não pode convidar o perfil com o id informado.')
+
+@api_view(['POST'])
+@renderer_classes((JSONRenderer, BrowsableAPIRenderer))
+def aceitar(request, *args, **kwargs):
+	perfil_logado = get_perfil_logado(request)
+	try:
+		convite = Convite.objects.filter(convidado=perfil_logado).get(id=kwargs['convite_id'])
+	except:
+		raise exceptions.NotFound('Não foi encontrado um convite com o informado')
+	convite.aceitar()
+	return response.Response({'menssagem': 'Convite aceito com sucesso.'},
+							  status=status.HTTP_201_CREATED)
+
+
+@api_view(['GET'])
+@renderer_classes((JSONRenderer, BrowsableAPIRenderer))
+def get_meu_perfil(request, *args, **kwargs):
+	perfil_logado = get_perfil_logado(request)
+	serializer = None
+	return response.Response(serializer.data, status=status.HTTP_200_OK)
+
+
 def get_perfil_logado(request):
    return request.user.perfil
 
-@login_required
-def aceitar(request, convite_id):
-    convite = Convite.objects.get(id=convite_id)
-    convite.aceitar()
-    return redirect('index')
