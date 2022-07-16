1ed9184c6c9e85ebd2b26e484f28081a1cad1c07
HackTestesIFB <caio.flausino@estudante.ifb.edu.br>
2022-07-16 20:15:52 -0300

    Parte 4 (incompleto)

--- src/Pages/Profiles/index.js
+++ src/Pages/Profiles/index.js
@@ -1,5 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { api } from "../../Services/api";
+import "./Profiles.css";
 
 export default function Profiles() {
-  return <div>Perfis</div>;
+
+  const [profiles, setProfiles] = useState();
+  const [currentProfile, setCurrentProfile] = useState();
+  const [message, setMessage] = useState();
+
+  useEffect(() => {
+    api.get('/perfil/')
+    .then((resp) => setCurrentProfile(resp.data))
+    .catch((error) => console.error(error));
+
+    api
+    .get('/perfis/')
+    .then((resp) => setProfiles(resp.data))
+    .catch((error) => console.error(error));
+
+  }, []);
+
+  function invite(id){
+    api.post(`/convites/convidar/${id}`)
+    .then((resp) => setMessage(resp.data))
+    .catch((error) => console.error(error));
+  }
+
+  return (
+  <div className="profiles">
+    <div className="invite">
+        {profiles?.map((profile) =>  
+            profile.id == currentProfile?.id ? null : (
+                <>
+                    <div className="card" key={profile.id}>
+                        <h3>{profile.nome}</h3>
+                        <span>{profile.email}</span>
+                        {profile.pode_convidar ? <button className="icon" title="convidar" onClick={() => invite(profile.id)}></button> : null}
+                    </div>
+                </>
+            ),
+        )}
+    </div>
+  </div>
+  );
 }
