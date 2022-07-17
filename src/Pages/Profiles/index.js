8fa858949eee28dbe11fbf9fc33543ba3a982564
HackTestesIFB <caio.flausino@estudante.ifb.edu.br>
2022-07-16 23:45:25 -0300

    Parte 4 completa

--- src/Pages/Profiles/index.js
+++ src/Pages/Profiles/index.js
@@ -7,6 +7,8 @@ export default function Profiles() {
   const [profiles, setProfiles] = useState();
   const [currentProfile, setCurrentProfile] = useState();
   const [message, setMessage] = useState();
+  const [currentInvitedProfile, setCurrentInvitedProfile] = useState();
+  const [invites, setInvites] = useState();
 
   useEffect(() => {
     api.get('/perfil/')
@@ -18,29 +20,55 @@ export default function Profiles() {
     .then((resp) => setProfiles(resp.data))
     .catch((error) => console.error(error));
 
+    api.get('/convites/')
+    .then(resp => setInvites(resp.data))
+    .catch((error) => console.error(error));
+
   }, []);
 
   function invite(id){
     api.post(`/convites/convidar/${id}`)
-    .then((resp) => setMessage(resp.data))
+    .then((resp) => setMessage(resp.data.messagem))
     .catch((error) => console.error(error));
+
+    setCurrentInvitedProfile(id);
   }
 
   return (
   <div className="profiles">
     <div className="invite">
         {profiles?.map((profile) =>  
-            profile.id == currentProfile?.id ? null : (
-                <>
-                    <div className="card" key={profile.id}>
+            profile.id === currentProfile?.id ? null : (
+                <div key={profile.id}>
+                    <div className="card">
                         <h3>{profile.nome}</h3>
                         <span>{profile.email}</span>
                         {profile.pode_convidar ? <button className="icon" title="convidar" onClick={() => invite(profile.id)}></button> : null}
                     </div>
-                </>
+                    {profile.id === currentInvitedProfile ? <span className="message">{message}</span> : null}
+                </div>
             ),
         )}
     </div>
+
+    <div className="invitations">
+        <h2>Convites</h2>
+        {invites?.map((invite) => (
+            <div className="card">
+                <h3>{invite.nome}</h3>
+                <button>Aceitar</button>
+            </div>
+        ))}
+    </div>
+
+    {/*<div>
+        {currentProfile?.contatos.map(contact => (
+            <div className="card" key={contact.id}>
+                <h3>{contact.nome}</h3>
+                <span>{contact.email}</span>
+            </div>
+        ))}
+    </div> */}
   </div>
   );
 }
